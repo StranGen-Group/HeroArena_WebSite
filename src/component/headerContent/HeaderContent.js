589cad1dd70b9e175dafd85448d8d285777a7bd1
Green-Blood <bloodjey98@gmail.com>
2025-12-08 08:25:41 +0500

    Update: Jenshina

--- src/component/headerContent/HeaderContent.js
+++ src/component/headerContent/HeaderContent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './HeaderContent.scss';
-import aboutImage from '../../assets/image/about/about-image.png';
+import aboutImage from '../../assets/image/about/mage.png';
 import { useLanguage } from '../../context/LanguageContext';
 import useInView from '../../hooks/useInView';
 import { ANIMATION_CONFIG, ANIMATION_CLASSES } from '../../constants/animation';
@@ -15,15 +15,15 @@ const HeaderContent = () => {
   return (
     <section className="about" id={SECTIONS.HOME}>
       <div className="about__content">
-        <div 
-          ref={titleRef} 
+        <div
+          ref={titleRef}
           className={`about__text-content ${titleInView ? `${ANIMATION_CLASSES.FADE_IN_LEFT} ${ANIMATION_CLASSES.ANIMATED}` : ANIMATION_CLASSES.HIDDEN}`}
         >
           <h2 className="about__title">
             {t('headerContentTitle')}
           </h2>
-          
-          <p 
+
+          <p
             ref={textRef}
             className={`about__description ${textInView ? `${ANIMATION_CLASSES.FADE_IN_LEFT} ${ANIMATION_CLASSES.ANIMATED}` : ANIMATION_CLASSES.HIDDEN}`}
             style={{ animationDelay: ANIMATION_CONFIG.DELAY.MEDIUM }}
@@ -32,14 +32,14 @@ const HeaderContent = () => {
           </p>
         </div>
 
-        <div 
+        <div
           ref={imageRef}
           className={`about__image-wrapper ${imageInView ? `${ANIMATION_CLASSES.FADE_IN_RIGHT} ${ANIMATION_CLASSES.ANIMATED}` : ANIMATION_CLASSES.HIDDEN}`}
           style={{ animationDelay: ANIMATION_CONFIG.DELAY.LONG }}
         >
-          <img 
-            src={aboutImage} 
-            alt="About our team" 
+          <img
+            src={aboutImage}
+            alt="About our team"
             className="about__image"
           />
         </div>
